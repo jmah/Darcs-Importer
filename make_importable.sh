6379f6305161918607a74b73495cfd3a8bc646ab
Jonathon Mah <jonathon@playhaus.org>
2005-09-22 10:39:29 -0700

    Added status output to make_importable.sh script, added mdimport as last step
    darcs-hash:20050922173929-5716c-c1ac4717c6588ee536bc09534fabd6eef38d5aae.gz

--- make_importable.sh
+++ make_importable.sh
@@ -10,8 +10,13 @@ else
 		echo "$0: $1: Directory not found"
 		exit 127
 	else
+		echo "$0: Creating: $1/Contents/"
 		mkdir "$1/Contents/"
+		echo "$0: Creating: $1/Contents/PkgInfo"
 		echo -n 'DRep????' > "$1/Contents/PkgInfo"
+		echo "$0: Setting bundle bit"
 		/Developer/Tools/SetFile -a B "$1"
+		echo "$0: Running mdimport"
+		mdimport "$1"
 	fi
 fi
